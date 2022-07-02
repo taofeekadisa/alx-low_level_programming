3fd62555504c911be3cb2e5dd814f4f4466fe6f9
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-01 20:57:16 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - Prints the numbers from 0 to 9
+* main - Prints the number from 0 to 9
 *
-* Return: Always 0 (success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
-		
-		putchar(c);
+
+	putchar(c);
 
 	putchar('\n');
 
