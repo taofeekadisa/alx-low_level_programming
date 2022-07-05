4edd360adb5874531cf218d324a208774b1bfbc5
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-05 13:09:33 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include *main.h*
+
+/**
+* main - print _putchar
+*
+* Return: Always 0 (Success)
+* */
+int main(void)
+{
+	_putcahr(95);
+	_putcahr(112);
+	_putcahr(117);
+	_putcahr(116);
+	_putcahr(99);
+	_putcahr(104);
+	_putcahr(97);
+	_putcahr(114);
+	_putcahr('\n');
+
+	return (0);
+}
