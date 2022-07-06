08078aca9a76566863e4de67b8f9f9f547717eed
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-05 18:28:22 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,13 +14,13 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minutes <= 59; munite++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((munite / 10) + '0');
-			_putchar((munite % 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minutte % 10) + '0');
 			_putchar('\n');
 		}
 	
