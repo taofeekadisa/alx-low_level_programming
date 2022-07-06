3a4645cc73762411c88212a4f4daa26616e984d4
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-05 17:14:42 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/*
+*File: is the function lowercase or upper
+*Auth: muhandasa
+*/
+#include "main.h"
+/**
+*_isalpha -check if a character is lowercase or uppercase.
+*@c: The character to be checked.
+*
+* Return: 1 if character is lowercase or uppercase, or otherwise.
+*/
