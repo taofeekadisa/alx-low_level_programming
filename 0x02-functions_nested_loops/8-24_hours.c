354ae8d166f6a4a0d311dc2f945166a56a103235
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-06 00:48:34 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
 
 #include "main.h"
 /**
-* jack_bauer - Prints every munites of the day of 
+* jack_bauer - Prints every munites of the day of
 *		Jack Bauer, strting from 00:00 to 23:59.
 */
 void jack_bauer(void)
