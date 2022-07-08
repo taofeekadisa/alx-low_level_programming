3faf0d5f4794f5f21851b9df65fff33da57aeb1f
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-08 03:14:54 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+/*
+* File: 10-print_traingle.c
+* Auth: muhandasa
+*/
+
+#include "main.h"
+
+/**
+* print_traingle - entry point
+*
+* Description: Prints diagonal
+* @size: size of the traingle
+* Return: void
+*/
+
+void print_traingle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
