20245fe737e76264934fdf36a12b45f2686dc816
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-07 20:54:05 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,8 +18,8 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-				_putchar((num/10) + '0');
-			-putchar((num % 10) + '0');
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
