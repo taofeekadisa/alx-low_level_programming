dcc06387f5da3b8d967ccdb0f602b98811a15ff8
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-08 02:31:36 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+/*
+* File: 8-print_sqaure.c
+* Auth: muhandasa
+*/
+
+#include "main.h"
+
+/**
+* print_square - prints a square of size
+* @size: size of the square
+*
+* Return: void
+*/
+
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
