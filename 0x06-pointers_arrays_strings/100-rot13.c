df48fc1fda6ec801f86cde3440644bc40d1cb489
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-14 21:31:53 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + 1))
+			{
+				*(s + 1) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
