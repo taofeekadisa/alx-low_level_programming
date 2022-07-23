2a07f5dd17e886c89ae93f24977fdedaf41fd0b5
muhandasa <adisataofeekabiodun@gmail.com>
2022-07-23 00:45:07 -0700

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - The entry point
+ * @argc: Argurmnt counter
+ * @argv: Array of pointer to strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,6 +6,6 @@
 #include <ctype.h>
 
 int _putchar(char c);
-int main(int argc, char* argv[]);
+int main(int argc, char *argv[]);
 
 #endif /*MAIN_H*/
