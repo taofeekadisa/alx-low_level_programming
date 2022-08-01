6a25f7a3c69d5940aa7009e8338ca8bab8bf97a9
muhandasa <adisataofeekabiodun@gmail.com>
2022-08-01 09:33:49 -0700

    structures_type

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			print("Name: (nil)\n");
+			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
